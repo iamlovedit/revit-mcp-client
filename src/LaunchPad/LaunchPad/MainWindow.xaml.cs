e7bef348701c9c02a14d8803cb611cb177c3bf34
Young <iamlovedit@163.com>
2025-08-15 15:03:33 +0800

    refactor project name

--- src/LaunchPad/LaunchPad/MainWindow.xaml.cs
+++ src/LaunchPad/LaunchPad/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Documents;
-namespace WpfApp1
+namespace LaunchPad
 {
 
     public partial class MainWindow : Window
